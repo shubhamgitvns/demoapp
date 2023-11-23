ab9f9020c25f5dabc56e560ca1bdc60cc5e1ef0e
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-11-23 17:09:22 +0530

    Understand Expanded widget

--- lib/UserHomePage.dart
+++ lib/UserHomePage.dart
@@ -137,7 +137,7 @@ class _LoaderPageState extends State<LoaderPage> {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   SizedBox(
-                                    width:250,
+                                    width: 250,
                                     child: RichText(
                                       text: const TextSpan(
                                         text: 'Welcome To ',
@@ -377,329 +377,57 @@ class _SecondPageState extends State<SecondPage> {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                  color: Colors.tealAccent,
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(75),
-                      bottomLeft: Radius.circular(75))),
-              child: Row(
-                //mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  //***************Img column***********************
-                  Column(
-                    children: [
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 10, right: 10, left: 10),
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.teal),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.black,
-                                offset: Offset(2.0, 2.0),
-                                blurRadius: 10,
-                                spreadRadius: 1.0,
-                              ),
-                              BoxShadow(
-                                color: Colors.transparent,
-                                offset: Offset(-2.0, -2.0),
-                                blurRadius: 10,
-                                spreadRadius: 1.0,
-                              ),
-                            ],
-                          ),
-                          child: FittedBox(
-                            alignment: Alignment.center,
-                            fit: BoxFit.contain,
-                            child: Image.network(
-                                "https://cdn.pixabay.com/photo/2016/01/08/20/02/michelle-obama-1129160_1280.jpg"),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: const [
-                          Center(
-                            child: Text(
-                              "Tanya Singh",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: const [
-                          Center(
-                            child: Text(
-                              "Advocate",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-
-                  //*****************Message column*************
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 150,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: Colors.black),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.black,
-                                offset: Offset(2.0, 2.0),
-                                blurRadius: 10,
-                                spreadRadius: 1.0,
-                              ),
-                              BoxShadow(
-                                color: Colors.transparent,
-                                offset: Offset(-2.0, -2.0),
-                                blurRadius: 10,
-                                spreadRadius: 1.0,
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            children: [
-                              RichText(
-                                text: const TextSpan(
-                                  text: 'Welcome To ',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                        text: 'Advocate',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.blue)),
-                                    TextSpan(text: ' App'),
-                                  ],
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Card(
-                                    child: Text("Latest news",
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.red)),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  SizedBox(
-                                    width: 250,
-                                    child: Card(
-                                      child: Text(
-                                          "Alert the crime Satark reho Crime se oe use kerne se  ndewdew fedjfdj fdf jd ffefejf effdfddvdnv  j jb jcbsj ",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black)),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-            //WhatsApp logo and link
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
+          child:  Column(
+            children: [
+              Container(
+                color: Colors.yellow,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(75),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black,
-                              offset: Offset(2.0, 2.0),
-                              blurRadius: 10,
-                              spreadRadius: 1.0,
-                            ),
-                            BoxShadow(
-                              color: Colors.transparent,
-                              offset: Offset(-2.0, -2.0),
-                              blurRadius: 10,
-                              spreadRadius: 1.0,
-                            ),
-                          ]),
-                      child: const Center(
-                        child: Text("img"),
+                    Expanded(
+                      flex:1,
+                      child: Container(
+                        color: Colors.red,
+                        child: Column(
+                          children: [
+                            Text("1")
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.green,
+                        child: Column(
+                          children: [
+
+                            Text("2"),
+                            Text("3")
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.blueAccent,
+                        child: Column(
+                          children: [
+                            Text("4"),
+                            Text("5"),
+                            Text("6")
+
+
+                          ],
+                        ),
                       ),
                     )
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Column(
-                    children: const [Text("link")],
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
+              )
+            ],
+          ),
 
-            //facebook logo and link
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(75),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black,
-                              offset: Offset(2.0, 2.0),
-                              blurRadius: 10,
-                              spreadRadius: 1.0,
-                            ),
-                            BoxShadow(
-                              color: Colors.transparent,
-                              offset: Offset(-2.0, -2.0),
-                              blurRadius: 10,
-                              spreadRadius: 1.0,
-                            ),
-                          ]),
-                      child: const Center(
-                        child: Text("img"),
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Column(
-                    children: const [Text("link")],
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-
-            //Twitter logo and link
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(75),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black,
-                              offset: Offset(2.0, 2.0),
-                              blurRadius: 10,
-                              spreadRadius: 1.0,
-                            ),
-                            BoxShadow(
-                              color: Colors.transparent,
-                              offset: Offset(-2.0, -2.0),
-                              blurRadius: 10,
-                              spreadRadius: 1.0,
-                            ),
-                          ]),
-                      child: Center(
-                        child: Text("img"),
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Column(
-                    children: const [Text("link")],
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-
-            //Lets go button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 50,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blueAccent,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 32, vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      child: Text("Let's -> go")),
-                )
-              ],
-            ),
-          ],
-        ),
       ),
     );
   }
